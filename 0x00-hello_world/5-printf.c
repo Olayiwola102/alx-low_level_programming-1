b6d5bebc1c745fa4bb02204ee63a5e685ea7933b
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-11 18:14:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
+ *main - entry point
  *
-   *main - entry point
- *
-    *
- *
-     *Return: O after printing the function
- *
-      */
+ *Return: O after printing the function
+ */
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
