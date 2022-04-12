621b7202b607225471c5eb92cab42cec7e8e701e
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-11 17:04:04 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *  *main - entry point
+ *   *
+ *    *Return: O after printing the function
+ *     */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
