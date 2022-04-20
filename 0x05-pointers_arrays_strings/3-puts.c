4a4ff85bc6ef69bfbdebf3e538b3247b23103b53
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-20 08:48:00 -0700

    Third task, 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - printing of string,  followed by a new line, to stdout
+ * @str: The character to be printed
+ * Return: If the output is correct
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+
+		i++;
+	}
+
+	_putchar('\n')'
+
+}
