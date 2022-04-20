df5d11ddb98bb1c3755244b79b9599c603d942a8
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-20 08:19:52 -0700

    Third Task, finding the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /**
  * _stren - finding the length of a string in a given value
  * @s: the pointer that will be updates
+ *
  * Return: void that the output is correct
  */
 
@@ -11,6 +12,5 @@ int _strlen(char *s)
 	int i = 0;
 	while (s[i])
 		i++;
-
 	return (i);
 }
