cbe03d19da90f7cda4dc39ab8dcf2551d3c65a8f
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-20 07:57:39 -0700

    Zero task of pointer.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
+#include <studio.h>
 
 /**
-*reset_to_98 - update an integer value to 98
-*@n: A pointer to an int that will be updated
-*
-*Return: void that means is correct
-*/
+ *  reset_to_98 - updates the value *n
+ *  @n: the number to modify.
+ *  Return: void means it is correct
+ */
 
 void reset_to_98(int *n)
-
 {
-*n = 98;
+		*n = 98;
 }
