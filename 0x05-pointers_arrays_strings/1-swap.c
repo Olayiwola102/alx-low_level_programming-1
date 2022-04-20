3a8efea4a30c8e1bf1a894def8e21147cf5f6c01
Abdulwaheed Musa <abdulwaheedmusa101.com>
2022-04-20 08:05:44 -0700

    first task, Swapping of two numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* swap_int - swapping of variables value of the integers
+* @a: A pointer to be swapped
+* @b: A pointer to be swapped
+* Return: void that means the answer is correct
+*/
+
+void swap_int(int *a, int *)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
